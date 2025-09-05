be69e09fe970c6af9886d787b5ed7fc53cdb6846
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-09-05 02:26:29 +0200

    Modified the middleware.

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,4 +1,6 @@
+import requests
 from django.http import HttpResponseForbidden
+from django.core.cache import cache
 from .models import RequestLog, BlockedIP
 
 class RequestLoggingMiddleware:
@@ -13,10 +15,28 @@ class RequestLoggingMiddleware:
         # Block if IP is blacklisted
         if BlockedIP.objects.filter(ip_address=ip).exists():
             return HttpResponseForbidden("Forbidden: Your IP has been blocked.")
-        
+
+        # Try to get geolocation from cache
+        geo_data = cache.get(ip)
+        if not geo_data:
+            try:
+                response = requests.get(f'https://ipapi.co/{ip}/json/')
+                geo_data = response.json()
+                # Cache the result for 24 hours (86400 seconds)
+                cache.set(ip, geo_data, 86400)
+            except requests.exceptions.RequestException:
+                geo_data = {}
+
+        country = geo_data.get('country_name', '') if geo_data else ''
+        city = geo_data.get('city', '') if geo_data else ''
 
         # Save to the database
-        RequestLog.objects.create(ip_address=ip, path=path)
+        RequestLog.objects.create(
+            ip_address=ip,
+            path=path,
+            country=country,
+            city=city
+        )
 
         # Continue to next middleware or view
         response = self.get_response(request)
